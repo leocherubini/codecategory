e0f355b2073ca6d25cc1240b8007453f3579fa07
Leonardo Cherubini <Leonardo Cherubini>
2016-10-25 01:38:12 -0200

    Refatoracao para repository

--- src/CodeCategory/Controllers/AdminCategoriesController.php
+++ src/CodeCategory/Controllers/AdminCategoriesController.php
@@ -3,6 +3,7 @@
 namespace CodePress\CodeCategory\Controllers;
 
 use CodePress\CodeCategory\Models\Category;
+use CodePress\CodeCategory\Repositories\CategoryRepository;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Routing\ResponseFactory;
 
@@ -10,46 +11,46 @@ class AdminCategoriesController extends Controller
 {
 
 	/**
-	 * @var Category
+	 * @var CategoryRepository
 	 */
-	private $categoryModel;
+	private $categoryRepository;
 
 	/**
 	 * @var Response
 	 */
 	private $response;
 
-	public function __construct(ResponseFactory $response, Category $categoryModel)
+	public function __construct(ResponseFactory $response, CategoryRepository $categoryRepository)
 	{
 		$this->response = $response;
-		$this->categoryModel = $categoryModel;
+		$this->categoryRepository = $categoryRepository;
 	}
 
 	public function index()
 	{
-		$categories = $this->categoryModel->all();
+		$categories = $this->categoryRepository->all();
 
 		return $this->response->view('codecategory::index', compact('categories'));
 	}
 
 	public function create()
 	{
-		$categories = $this->categoryModel->all();
+		$categories = $this->categoryRepository->all();
 
 		return view('codecategory::create', compact('categories'));
 	}
 
 	public function store(Request $request)
 	{
-		$this->categoryModel->create($request->all());
+		$this->categoryRepository->create($request->all());
 
 		return redirect()->route('admin.categories.index');
 	}
 
 	public function edit($id)
 	{
-		$categories = $this->categoryModel->all();
-		$category = $this->categoryModel->find($id);
+		$categories = $this->categoryRepository->all();
+		$category = $this->categoryRepository->find($id);
 
 		return view('codecategory::edit', compact('categories', 'category'));
 	}
@@ -60,14 +61,14 @@ class AdminCategoriesController extends Controller
 		if(!isset($data['active']))
 			$data['active'] = 0;
 
-		$this->categoryModel->find($id)->update($data);
+		$this->categoryRepository->update($data, $id);
 
 		return redirect()->route('admin.categories.index');
 	}
 
 	public function destroy($id)
 	{
-		$this->categoryModel->find($id)->delete();
+		$this->categoryRepository->delete($id);
 
 		return redirect()->route('admin.categories.index');
 	}
--- tests/CodeCategory/Controllers/AdminCategoriesControllerTest.php
+++ tests/CodeCategory/Controllers/AdminCategoriesControllerTest.php
@@ -6,6 +6,7 @@ use CodePress\CodeCategory\Tests\AbstractTestCase;
 use CodePress\CodeCategory\Controllers\AdminCategoriesController;
 use CodePress\CodeCategory\Controllers\Controller;
 use CodePress\CodeCategory\Models\Category;
+use CodePress\CodeCategory\Repositories\CategoryRepository;
 use Mockery as m;
 use Illuminate\Contracts\Routing\ResponseFactory;
 
@@ -14,22 +15,22 @@ class AdminCategoriesControllerTest extends AbstractTestCase
 
 	public function test_should_extend_from_controller()
 	{
-		$category = m::mock(Category::class);
+		$categoryRepository = m::mock(CategoryRepository::class);
 		$responseFactory = m::mock(ResponseFactory::class);
-		$controller = new AdminCategoriesController($responseFactory, $category);
+		$controller = new AdminCategoriesController($responseFactory, $categoryRepository);
 
 		$this->assertInstanceOf(Controller::class, $controller);
 	}
 
 	public function test_controller_should_run_index_method_and_return_correct_arguments()
 	{
-		$category = m::mock(Category::class);
+		$categoryRepository = m::mock(CategoryRepository::class);
 		$responseFactory = m::mock(ResponseFactory::class);
-		$controller = new AdminCategoriesController($responseFactory, $category);
+		$controller = new AdminCategoriesController($responseFactory, $categoryRepository);
 		$html = m::mock();
 
 		$categoriesResult = ['cat1', 'cat2'];
-		$category->shouldReceive('all')->andReturn($categoriesResult);
+		$categoryRepository->shouldReceive('all')->andReturn($categoriesResult);
 
 		$responseFactory->shouldReceive('view')
 			->with('codecategory::index', ['categories'=>$categoriesResult])
